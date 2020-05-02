d251a1445b01317b2b9f7eefec6d1c9728f9cadc
minam An <minamhunmin@gmail.com>
2020-05-02 10:31:58 +0900

    add Module time

---
+++ src/time.js
@@ -0,0 +1,26 @@
+const elTime = document.querySelector('.time');
+
+const getFormatTime = (time) => {
+    
+}
+
+const getNowTime = () => {
+    const t = new Date();
+
+    let YYYY = t.getFullYear();
+    let MM = t.getMonth() + 1;
+    let DD = t.getDate();
+    let hh = t.getHours();
+    let mm = t.getMinutes();
+    let ss = t.getSeconds();
+    
+    const date = `${YYYY}-${MM < 10? `0${MM}` : MM }-${DD < 10? `0${DD}` : DD }`;
+    const time = `${hh < 10? `0${hh}` : hh }:${mm < 10? `0${mm}` : mm }:${ss < 10? `0${ss}` : ss }`;
+    return date +' '+ time;
+}
+
+export const init = () => {
+    setInterval(() => {
+        elTime.innerText = getNowTime();
+    }, 1000);
+}
